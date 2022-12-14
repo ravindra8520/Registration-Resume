6d45837d9186a8566fa44138f37793be423234bb
ravindra8520 <120161123+ravindra8520@users.noreply.github.com>
2022-12-14 10:14:46 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,11 @@
+/*function genPdf2()
+{
+   html2canvas(document.getElementById('textDiv"),{
+       onrendered: function (canvas){
+        var image=canvas.toDataURL("img/jpg");
+        var doc=new jspdf9);
+        doc.addImage(image,'JPG',20,20);
+        doc.save("test.pdf");
+      }
+    });
+  }*/
